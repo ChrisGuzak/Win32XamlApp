58357d29fdef59a1985385b1c87a95846b1c9407
Chris Guzak <chrisg@ntdev.microsoft.com>
2021-07-01 21:44:36 -0700

    C++ types, winuser.h

--- inc/XamlWin32Helpers.h
+++ inc/XamlWin32Helpers.h
@@ -1,16 +1,17 @@
 #pragma once
+#include <winuser.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.UI.Xaml.h>
 
-inline PCWSTR MakeIntResourceId(int i)
+inline const wchar_t* MakeIntResourceId(int i)
 {
-    return reinterpret_cast<PCWSTR>(static_cast<ULONG_PTR>(i));
+    return reinterpret_cast<const wchar_t*>(static_cast<ULONG_PTR>(i));
 }
 
 #define XAMLRESOURCE  256 // see resource.h, try to get rid of this by using a standard "file" type.
 
 template<typename T = winrt::Windows::UI::Xaml::UIElement>
-winrt::Windows::UI::Xaml::UIElement LoadXamlResource(HMODULE mod, uint32_t id, PCWSTR type = MakeIntResourceId(XAMLRESOURCE))
+winrt::Windows::UI::Xaml::UIElement LoadXamlResource(HMODULE mod, uint32_t id, const wchar_t* type = MakeIntResourceId(XAMLRESOURCE))
 {
     auto rc = ::FindResourceW(mod, MakeIntResourceId(id), type);
     winrt::check_bool(rc == nullptr);
@@ -23,7 +24,7 @@ winrt::Windows::UI::Xaml::UIElement LoadXamlResource(HMODULE mod, uint32_t id, P
 }
 
 template <typename T>
-void RegisterWindowClass(PCWSTR windowClassName)
+void RegisterWindowClass(const wchar_t* windowClassName)
 {
     WNDCLASSEXW wcex{ sizeof(wcex) };
     wcex.style = CS_HREDRAW | CS_VREDRAW;
@@ -49,7 +50,7 @@ void RegisterWindowClass(PCWSTR windowClassName)
     };
     wcex.hInstance = wil::GetModuleInstanceHandle();
     auto imageResMod = LoadLibraryExW(L"imageres.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32 | LOAD_LIBRARY_AS_DATAFILE);
-    wcex.hIcon = LoadIconW(imageResMod, reinterpret_cast<PCWSTR>(5206)); // App Icon
+    wcex.hIcon = LoadIconW(imageResMod, reinterpret_cast<const wchar_t*>(5206)); // App Icon
     wcex.hCursor = LoadCursorW(nullptr, IDC_ARROW);
     wcex.lpszClassName = windowClassName;
 
